12268ca8d050d7d47b8e920a2fe523b429d24fb3
Jeremy Bass <jeremybass@cableone.net>
2014-10-10 11:44:33 -0700

    minor changes to the instruction area

--- page-generalga.php
+++ page-generalga.php
@@ -6,8 +6,8 @@ $themePath = get_stylesheet_directory_uri(); // we want the child theme url
 $code = get_gauntlet_attr("code");
 ?>
 <?php get_header(); ?>
+<?php get_template_part('parts/globals/instructions'); ?>
 <main>
-	<?php get_template_part('parts/globals/instructions'); ?>
 	<section class="row side-right gutter pad-bottom tall">
 		<div class="column one">
 			<?php get_template_part('parts/headers/'.$code); ?>
