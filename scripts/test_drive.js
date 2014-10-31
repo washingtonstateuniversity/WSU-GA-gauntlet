b57619f32fd567c0808c4fa45cb573673e6f0793
Jeremy Bass <jeremybass@cableone.net>
2014-10-31 10:20:48 -0700

    sets up an object holding al the test unit items ready to go

--- scripts/test_drive.js
+++ scripts/test_drive.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+var targs={};
+$(function(){
+	$.each($('.test_unit'), function(){
+		$.each($('a,input,.unit_item',this), function(){
+			var tar=$(this);
+			tar.uniqueId();
+			var obj=[];
+			var action;
+			if(tar.is("a")){
+				action="click";
+			}
+			if(tar.is("input")){
+				action="change";
+			}
+			if(tar.is(".unit_item")){
+				action=tar.data('test_action');
+			}
+			obj[tar.attr('id')]={
+				action:action
+			}
+			tagrs = $.extend(targs,obj);
+		});
+	});
+});
